742536aa85e2c918806351c389b621b1f0fae334
Saud122 <58867506+Saud122@users.noreply.github.com>
2024-05-30 21:40:04 +0300

    Saud_A

---
+++ streamlit_app.py
@@ -0,0 +1,147 @@
+import streamlit as st
+
+
+st.write("if you would like to see the streamlit web, please copy this link: https://use-case-5-39e3nrfrxck3whxefuvujd.streamlit.app/  ")
+
+st.text("Welcome to My Streamlit website!")
+
+# Markdown text
+st.markdown("""
+    ## Topic: Jadarat
+
+    <span style='color:black; font-weight:bold;'>
+    Jadarat.sa is a platform dedicated to fostering growth, development, 
+    and empowerment. their mission is to equip individuals, organizations, 
+    and communities with the skills, knowledge, and resources needed to thrive in today's dynamic world.
+    </span>
+
+
+""", unsafe_allow_html=True)
+
+st.markdown("\n \n")
+
+st.text(" Here are the Five questions:  ")
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+
+st.markdown("""
+    <span style='color:black; font-weight:bold;'> Q1)  What proportion of job postings is attributed to each region within the kingdom?</span>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+            
+            <span style='color:black; font-weight:bold;'>
+            Understanding regional job posting proportions in Saudi Arabia helps job seekers target their search efforts more effectively, 
+            increasing their chances of finding relevant opportunities in areas with higher demand for their skills, 
+            thus saving time and effort. </span>
+            """, unsafe_allow_html=True )
+
+
+st.markdown("\n \n")
+
+st.markdown("\n \n")
+
+# Display an image from a local file
+st.image('Minutes played.png', caption='Minutes played.png', use_column_width=True)
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+st.markdown("""
+    <span style='color:black; font-weight:bold;'> Q2) Is there a gender preference indicated in the job postings?</span>
+""", unsafe_allow_html=True)
+
+
+st.markdown("\n \n")
+
+st.markdown("""
+            <span style='color:black; font-weight:bold;'> Identifying if there's a gender preference specified in job postings allows job seekers to navigate towards workplaces
+            that prioritize diversity and inclusion, fostering environments where all individuals have equal opportunities for employment
+            and advancement.</span>
+            """, unsafe_allow_html=True )
+
+
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+
+# Display an image from a local file
+st.image('elbow method.png', caption='elbow method.png', use_column_width=True)
+
+
+
+
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+st.markdown("""
+    <span style='color:black; font-weight:bold;'> Q3) What is the expected salary range for fresh graduates?</span>
+""", unsafe_allow_html=True)
+
+
+st.markdown("\n \n")
+
+st.markdown("""
+            <span style='color:black; font-weight:bold;'> Understanding the expected salary range for fresh graduates is crucial
+             because it helps them set realistic salary expectations and negotiate fair compensation during job interviews,
+             ensuring they receive appropriate remuneration for their qualifications and skills.</span>
+            """, unsafe_allow_html=True )
+
+
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+
+# Display an image from a local file
+st.image('K-means clustering.png', caption='K-means clustering.png', use_column_width=True)
+
+
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+st.markdown("""
+    <span style='color:black; font-weight:bold;'> Q4: Are job opportunities predominantly targeted at individuals with experience, or is there room for fresh graduates as well?</span>
+""", unsafe_allow_html=True)
+
+
+st.markdown("\n \n")
+
+st.markdown("""
+            <span style='color:black; font-weight:bold;'> Based on the chart below it's targeting fresh graduates with no prior experience.</span>
+            """, unsafe_allow_html=True )
+
+
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+
+# Display an image from a local file
+st.image('scatter plot.png', caption='scatter plot.png', use_column_width=True)
+
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+st.markdown("""
+    <span style='color:black; font-weight:bold;'> Q5: Which region has the highest job postings in Saudi Arabia for fresh graduates with no experience?</span>
+""", unsafe_allow_html=True)
+
+
+st.markdown("\n \n")
+
+st.markdown("""
+            <span style='color:black; font-weight:bold;'> The region that has the highest job postings in Saudi Arabia for fresh graduates with no experience is shown in the chart below. </span>
+            """, unsafe_allow_html=True )
+
+
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
